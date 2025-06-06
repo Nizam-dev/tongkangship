822b02e922277bf405b8d435b140bb0e15bf5d81
Nizam-dev <mahfudzkhoirunnizam@gmail.com>
2025-06-06 22:59:27 +0700

    rev1-tbeli

--- resources/views/user/t_beli.blade.php
+++ resources/views/user/t_beli.blade.php
@@ -32,12 +32,27 @@
                                     @endif
                                 </td>
                                 <td>
-                                    @if($item->status == 'pembayaran')
+                                    <!-- @if($item->status == 'pembayaran')
                                     <a href="{{ url('pembayaran/'.$item->id) }}"
                                         class="btn btn-sm btn-warning">Pembayaran</a>
                                     @elseif($item->status == 'expired' || $item->status == 'pembayaran ditolak' || $item->status == 'menunggu konfirmasi')
                                     -
                                     @endif
+ -->
+
+                                      @if($item->status == 'pembayaran')
+                                        <a href="{{ url('pembayaran/'.$item->id) }}" class="btn btn-sm btn-warning">Pembayaran</a>
+                                        @elseif($item->status == 'expired' || $item->status == 'pembayaran ditolak' || $item->status ==
+                                        'menunggu konfirmasi')
+                                        -
+                                        @else
+                                        <a href="#" data-bs-toggle="modal" data-bs-target="#detail{{ $item->id }}"
+                                            class="btn btn-sm btn-primary">Detail</a>
+                                            <a href="{{ url('tracking/'.$item->id) }}" class="btn btn-sm btn-warning">
+                                                Tracking
+                                            </a>
+                                        @endif
+
                                 </td>
                             </tr>
                             @empty
@@ -48,3 +63,73 @@
                         </tbody>
                     </table>
                 </div>
+
+
+   @foreach ($data->where('type_booking', 'beli') as $item)
+  <!-- MOdal Detal -->
+  <div class="modal fade" id="detail{{ $item->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Detail Transaksi</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                  <table class="table table-hover table-stripped table-bordered">
+                      <tr>
+                          <td>Kode Booking</td>
+                          <td>: {{ $item->kode_booking }}</td>
+                      </tr>
+                      <tr>
+                          <td>Nama Kapal</td>
+                          <td>: {{ $item->nama_kapal }}</td>
+                      </tr>
+                      <tr>
+                          <td>Kategori</td>
+                          <td>: {{ $item->kategori }}</td>
+                      </tr>
+                      <tr>
+                          <td>Harga</td>
+                          <td>: @formatRupiah($item->total)</td>
+                      </tr>
+
+                      <tr>
+                          <td>Nama</td>
+                          <td>: {{ $item->nama }}</td>
+                      </tr>
+                      <tr>
+                          <td>Email</td>
+                          <td>: {{ $item->email }}</td>
+                      </tr>
+                      <tr>
+                          <td>No Hp</td>
+                          <td>: {{ $item->no_hp }}</td>
+                      </tr>
+                      <tr>
+                          <td>Alamat</td>
+                          <td>: {{ $item->alamat }}</td>
+                      </tr>
+                    <tr>
+                        <td>Pelabuhan Asal</td>
+                        <td>: {{ $item->pelabuhan_asal }}</td>
+                      </tr>
+                    <tr>
+                        <td>Pelabuhan Tujuan</td>
+                        <td>: {{ $item->pelabuhan_tujuan . ' - ' . $item->lokasi_pelabuhan }}</td>
+                      </tr>
+                      @if($item->bukti_pembayaran != null)
+                      <tr>
+                          <td>Bukti Pembayaran</td>
+                          <td>
+                              <a href="{{ url('images/bukti_pembayaran/'.$item->bukti_pembayaran) }}" class="text-primary">Lihat Pembayaran</a>
+                          </td>
+                      </tr>
+                      @endif
+
+                  </table>
+              </div>
+          </div>
+      </div>
+  </div>
+  @endforeach
